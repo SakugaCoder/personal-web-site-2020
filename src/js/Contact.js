26dbd8bd82308f7bc867b31471eccb03f7cdec62
Diego Fernando Bocanegra Trendo <diego.b@ppsti.com>
2020-08-06 12:28:56 -0500

    Fix favicon and contact form

--- src/js/Contact.js
+++ src/js/Contact.js
@@ -17,17 +17,18 @@ function Contact(){
             <div className="title">
                 <h2>{t('contact-me')}</h2>
             </div>
-            <form name="contact" netlify>
+            <form name="contact" action="mailto:diego.bocanegra.trenado@gmail.com"  method="post" enctype="text/plain">
                 <div className="main-data">
                     <input type="text" id="name" name="name" placeholder={t('name')}  onChange={handleChange}></input>
-                    <input type="email" id="email" name="email" placeholder={t('email')} onChange={handleChange}></input>
+                    <input type="email" id="mail" name="mail" placeholder={t('email')} onChange={handleChange}></input>
                 </div>
                 <div className="message">
-                    <textarea id="message" name="message" placeholder={t('message')} onChange={handleChange}></textarea>
+                    <textarea type="text" id="comment" name="comment" placeholder={t('message')} onChange={handleChange}></textarea>
                 </div>
 
                 <div className="btn-form">
                     <button type="submit" id="btnSend">{t('send')}</button>
+                    <button type="reset" name="reset" id="btnReset">Reset</button> 
                 </div>
             </form>
         </div>
