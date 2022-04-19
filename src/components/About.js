de2accb17a7f8a2e4578f782922427b8142c241c
Diego Fernando Bocanegra Trendo <diego.b@ppsti.com>
2022-04-19 00:15:43 -0500

    Add basic scroll reveal animation

--- src/components/About.js
+++ src/components/About.js
@@ -2,6 +2,8 @@ import React from 'react';
 import '../scss/About.scss';
 import Title from './Title';
 import { useTranslation } from 'react-i18next';
+import Fade from 'react-reveal/Fade';
+
 
 function About(){
     const { t } = useTranslation();
@@ -9,36 +11,40 @@ function About(){
         <div className="about" id="about">
             <div className="content">
                 <div className="about-content">
-                    <div className="personal-picture img-circle"></div> 
-                    <div className="text-content">
-                        <div className="section-title">
-                            <Title text={t('about-title')} />
+                    <Fade bottom delay={500}>
+                        <div className="personal-picture img-circle"></div> 
+                    </Fade>
+                    <Fade bottom delay={700}>
+                        <div className="text-content">
+                            <div className="section-title">
+                                <Title text={t('about-title')} />
+                            </div>
+                            <p>
+                                {t('about-me-1')}
+                            </p>
+
+                            <p>
+                                {t('about-me-2')}
+                            </p>
+
+                            <p>
+                                {t('about-me-3')}
+                            </p>
+
+                            <div className='tech-stack-list'>
+                                <ul>
+                                    <li>HTML</li>
+                                    <li>CSS</li>
+                                    <li>WordPress</li>
+                                </ul>
+                                <ul>
+                                    <li>JavaScript (ES6)</li>
+                                    <li>React</li>
+                                    <li>Node.Js</li>
+                                </ul>
+                            </div>
                         </div>
-                        <p>
-                            {t('about-me-1')}
-                        </p>
-
-                        <p>
-                            {t('about-me-2')}
-                        </p>
-
-                        <p>
-                            {t('about-me-3')}
-                        </p>
-
-                        <div className='tech-stack-list'>
-                            <ul>
-                                <li>HTML</li>
-                                <li>CSS</li>
-                                <li>WordPress</li>
-                            </ul>
-                            <ul>
-                                <li>JavaScript (ES6)</li>
-                                <li>React</li>
-                                <li>Node.Js</li>
-                            </ul>
-                        </div>
-                    </div>
+                    </Fade>
                 </div>
                 
                 
